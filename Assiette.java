795eda165ead520b198d27b9937bd454ab3d03f7
Abdellaoui-abdelwaheb <mamamamawahebabdellaoui@gmail.com>
2024-11-25 22:29:12 +0100

    Add files via upload

---
+++ Assiette.java
@@ -0,0 +1,11 @@
+
+abstract class Assiette extends Ustensile {
+
+
+    public Assiette(int anneeFabrication) {
+        super(anneeFabrication);
+    }
+
+
+    public abstract double calculerSurface();
+}
